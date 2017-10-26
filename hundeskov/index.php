aa6f8cb697453367b60d3e8d09412b848c933bed
kfkrab <kfkrab@gmail.com>
2017-10-26 08:33:03 +0200

    undermappe

---
+++ hundeskov/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Group 10's absence list</title>
+</head>
+
+<body>
+
+<h1>Hello, World!</h1>
+<h2>Krab made this</h2>
+
+</body>
+</html>
