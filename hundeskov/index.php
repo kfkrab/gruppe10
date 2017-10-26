8dabb77ab812f0f8aec0d7bffb0e2d2139c2ec70
kfkrab <kfkrab@gmail.com>
2017-10-26 10:12:54 +0200

    Done

--- hundeskov/index.php
+++ hundeskov/index.php
@@ -1,31 +1,67 @@
-<!doctype html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Group 10's liste over hundeskove i Odense</title>
+   <head>
+      <title>Angular JS Includes</title>
+
+      <style>
+         table, th , td {
+            border: 1px solid grey;
+            border-collapse: collapse;
+            padding: 5px;
+         }
+
+         table tr:nth-child(odd) {
+            background-color: #f2f2f2;
+         }
+
+         table tr:nth-child(even) {
+            background-color: #ffffff;
+         }
+      </style>
+          <script data-require="angular.js@1.2.x" src="http://code.angularjs.org/1.2.15/angular.js" data-semver="1.2.15"></script>
+
+   </head>
+   <body>
+      <h2>AngularJS that reads open data sets </h2>
+      <a href="https://portal.opendata.dk/dataset/62fe5392-9bb6-49c5-ba41-3a1b0297a3c2/resource/37816225-3471-4fd5-a855-66b9780baf8d/download/hundeskoveoghundeparker.json">Data source</a>
+      <div ng-app = "getData" ng-controller = "dataController">
+
+         <table>
+            <tr>
+               <th>Id</th>
+               <th>Placering</th>
+               <th>Koordinat</th>
+            </tr>
+
+            <tr ng-repeat = "x in dataset">
+               <td>{{ x.properties.id }}</td>
+               <td>{{ x.properties.placering }}</td>
+               <td>{{ x.properties.point }}</td>
+            </tr>
+         </table>
+      </div>
+
+      <script>
+      (function() {
+         var app = angular.module('getData', []);
+
+         app.controller('dataController', function($scope,$http){
+           // json dataset from opendata.dk
+           var url = "https://portal.opendata.dk/dataset/62fe5392-9bb6-49c5-ba41-3a1b0297a3c2/resource/37816225-3471-4fd5-a855-66b9780baf8d/download/hundeskoveoghundeparker.json";
+
+           $http.get(url).success( function(response) {
+              $scope.dataset = response.features;
+
+           });
+         });
 
 
-<!--CSS-->
-<link rel="stylesheet" type="text/css" href="css/style.css" />
-
-
-<!--angular-->
-<script src="angular.min.js"></script>
-
-
-<!--function-->	
-<script src="js/functions.php"></script>
-
-</head>
-
-<body ng-controller="StudentController as list">
-
-<p ng-repeat="students in studentList  ">{{hundeskov.navn}}
-<span ng-show="students.absence" style="color: red; font-size: 0.5em ">{{hundeskov.navn}}</span>
-<span ng-hide="students.absence" style="color: green; font-size: 0.5em ">{{hundeskov.beskrivelse}}</span>
-</p>
 
 
 
-</body>
+   })();
+      </script>
+
+
+
+   </body>
 </html>
--- hundeskov/js/functions.php
+++ hundeskov/js/functions.php
@@ -1,31 +1,67 @@
-<?php
+<html>
+   <head>
+      <title>Angular JS Includes</title>
 
-	define("JSONHUNDESKOV", "json/hundeskove.json");
+      <style>
+         table, th , td {
+            border: 1px solid grey;
+            border-collapse: collapse;
+            padding: 5px;
+         }
 
-	function getJsonData(){
-		//læs json filen
-		$content = file_get_contents(JSONHUNDESKOV);
-		//afkode json
-		$json = json_decode($content);
-		return $json->skov;
-    }
-?>
+         table tr:nth-child(odd) {
+            background-color: #f2f2f2;
+         }
 
-<script>
-(function(){
-	
-var app = angular.module('absenceList',[]);	
-	
-	app.controller('StudentController', function($scope){
-		$scope.studentList = students;
-	});
-		
-var students = [
-	{navn: '$skov->placering', beskrivelse: '$skov->beskrivelse'},
-	
-];
-				  
-	console.log(students);
-})();
-</script>
+         table tr:nth-child(even) {
+            background-color: #ffffff;
+         }
+      </style>
+          <script data-require="angular.js@1.2.x" src="http://code.angularjs.org/1.2.15/angular.js" data-semver="1.2.15"></script>
 
+   </head>
+   <body>
+      <h2>AngularJS that reads open data sets </h2>
+      <a href="https://portal.opendata.dk/dataset/730994e2-de08-46c6-bd1a-54084c53cd28/resource/4b4ce643-abe9-4968-932a-f71572e5d150/download/bycykelparkering.json">Data source</a>
+      <div ng-app = "getData" ng-controller = "dataController">
+
+         <table>
+            <tr>
+               <th>Id</th>
+               <th>Placering</th>
+               <th>Antal cykler</th>
+            </tr>
+
+            <tr ng-repeat = "x in dataset">
+               <td>{{ x.properties.id }}</td>
+               <td>{{ x.properties.placering }}</td>
+               <td>{{ x.properties.antal_cykler }}</td>
+            </tr>
+         </table>
+      </div>
+
+      <script>
+      (function() {
+         var app = angular.module('getData', []);
+
+         app.controller('dataController', function($scope,$http){
+           // json dataset from opendata.dk
+           var url = "https://portal.opendata.dk/dataset/730994e2-de08-46c6-bd1a-54084c53cd28/resource/4b4ce643-abe9-4968-932a-f71572e5d150/download/bycykelparkering.json";
+
+           $http.get(url).success( function(response) {
+              $scope.dataset = response.features;
+
+           });
+         });
+
+
+
+
+
+   })();
+      </script>
+
+
+
+   </body>
+</html>
